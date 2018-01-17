fd809750eb08c8465681839a0a908824ef10e964
Jordan Schelew <jas8522@gmail.com>
2018-01-17 13:01:08 -0400

    Provide useful info in error
    So the dev can know what they’re working with.

--- tpbbgmap/includes/frontend.php
+++ tpbbgmap/includes/frontend.php
@@ -176,7 +176,7 @@ else{
 							})[0];
 							
 						} else {
-							console.log('Geocode was not successful for the following reason: ' + status); //quieter error
+							console.log('Geocode was not successful with address "' + marker.address + '" for the following reason: ' + status); //quieter error
 						}
 					});
 						
